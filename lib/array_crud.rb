8726289a19f124bcbf2ff0df9544df748ca3dca1
John Sime <johnc.sime@gmail.com>
2018-10-02 06:05:43 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -14,25 +14,31 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
+  names.unshift("Joker")
 
 end
 
 def remove_element_from_end_of_array(array)
+  names.pop("James")
 
 end
 
 def remove_element_from_start_of_array(array)
+  names.shift("Joker")
 
 end
 
 def retrieve_element_from_index(array, index_number)
+  names(2)
 
 end
 
 def retrieve_first_element_from_array(array)
+  names(0)
 
 end
 
 def retrieve_last_element_from_array(array)
+  names(4)
 
 end
